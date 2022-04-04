ba6d19b4e6deae8af0d02fa9e9732f30c625dd69
dmaur <deepak@iccdeveloper.com>
2022-04-05 00:33:05 +0530

    - fixed category list click event.

--- app/src/main/java/com/example/sdd/CategoryFragment.java
+++ app/src/main/java/com/example/sdd/CategoryFragment.java
@@ -13,21 +13,37 @@ import androidx.fragment.app.Fragment;
 public class CategoryFragment extends Fragment {
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view= inflater.inflate(R.layout.fragment_category, container, false);
-        Button cat1=(Button) view.findViewById(R.id.cat1);
-        cat1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i= new Intent(getActivity(), HireDesigner.class);
-                startActivity(i);
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        Button cat1 = view.findViewById(R.id.cat1);
+        Button cat2 = view.findViewById(R.id.cat2);
+        Button cat3 = view.findViewById(R.id.cat3);
+        Button cat4 = view.findViewById(R.id.cat4);
+        Button cat5 = view.findViewById(R.id.cat5);
+        Button cat6 = view.findViewById(R.id.cat6);
+        Button cat7 = view.findViewById(R.id.cat7);
+        Button cat8 = view.findViewById(R.id.cat8);
+        Button cat9 = view.findViewById(R.id.cat9);
+        Button cat10 = view.findViewById(R.id.cat10);
+        cat1.setOnClickListener(v -> viewCategory(v));
+        cat2.setOnClickListener(v -> viewCategory(v));
+        cat3.setOnClickListener(v -> viewCategory(v));
+        cat4.setOnClickListener(v -> viewCategory(v));
+        cat5.setOnClickListener(v -> viewCategory(v));
+        cat6.setOnClickListener(v -> viewCategory(v));
+        cat7.setOnClickListener(v -> viewCategory(v));
+        cat8.setOnClickListener(v -> viewCategory(v));
+        cat9.setOnClickListener(v -> viewCategory(v));
+        cat10.setOnClickListener(v -> viewCategory(v));
         return view;
+    }
+
+    public void viewCategory(View v) {
+        Intent i = new Intent(getActivity(), HireDesigner.class);
+        startActivity(i);
     }
 }
