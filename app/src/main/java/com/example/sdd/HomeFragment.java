e58629bbe6d4cb975483f2a185fda150a1df49e2
dmaur <deepak@iccdeveloper.com>
2022-04-05 13:49:38 +0530

    - added third view profile click listener.

--- app/src/main/java/com/example/sdd/HomeFragment.java
+++ app/src/main/java/com/example/sdd/HomeFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class HomeFragment extends Fragment {
@@ -22,11 +24,22 @@ public class HomeFragment extends Fragment {
         viewprofile.setOnClickListener(v -> designerActivity());
         Button view_hire2 = view.findViewById(R.id.view_hire2);
         view_hire2.setOnClickListener(v -> designerActivity());
+        Button view_hire3= view.findViewById(R.id.view_hire3);
+        view_hire3.setOnClickListener(v -> designerActivity());
+        ImageView like= view.findViewById(R.id.like);
+        ImageView like2= view.findViewById(R.id.like2);
+        ImageView like3= view.findViewById(R.id.like3);
+        like.setOnClickListener(v -> showMessage());
+        like2.setOnClickListener(v -> showMessage());
+        like3.setOnClickListener(v -> showMessage());
         return view;
     }
 
     public void designerActivity() {
         Intent i = new Intent(getActivity(), Designercustomerview.class);
         startActivity(i);
+    }
+    public void showMessage(){
+        Toast.makeText(getActivity(),"Yaay! Liked",Toast.LENGTH_LONG).show();
     }
 }
